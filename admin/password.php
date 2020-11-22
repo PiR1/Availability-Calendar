7456d7084182819e0ada28b052a1e1d16364fe63
PiR <pirddeveloppeur@gmail.com>
2020-11-22 17:07:43 +0100

    Update form action url

--- admin/password.php
+++ admin/password.php
@@ -36,7 +36,7 @@ if (!(new authController())->checkAuth()){
                 <div class="card-header">Login</div>
 
                 <div class="card-body">
-                    <form method="POST" action="php/user/password">
+                    <form method="POST" action="api/user/password">
                         <div class="form-group row">
                             <label for="oldPassword" class="col-md-4 col-form-label text-md-right">Old Password</label>
                             <div class="col-md-6">
@@ -74,6 +74,6 @@ if (!(new authController())->checkAuth()){
 <script src="https://code.jquery.com/jquery-3.5.1.min.js"
         integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
         crossorigin="anonymous"></script>
-<script src="../assets/js/script.js"></script>
+<script src="../assets/js/script-dist.js"></script>
 </body>
 </html>
--- admin/register.php
+++ admin/register.php
@@ -38,7 +38,7 @@ if (!(new authController())->checkAuth()){
                 <div class="card-header">Register</div>
 
                 <div class="card-body">
-                    <form method="POST" action="php/user/signup">
+                    <form method="POST" action="api/user/signup">
                         <div class="form-group row">
                             <label for="username" class="col-md-4 col-form-label text-md-right">Username</label>
 
@@ -73,7 +73,7 @@ if (!(new authController())->checkAuth()){
 <script src="https://code.jquery.com/jquery-3.5.1.min.js"
         integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
         crossorigin="anonymous"></script>
-<script src="../assets/js/script.js"></script>
+<script src="../assets/js/script-dist.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
         integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
         crossorigin="anonymous"></script>
